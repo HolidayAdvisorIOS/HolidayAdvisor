80e7d187b413512c123e5741953dbb682b48371c
Iliyan Gogov <iliangogov@gmail.com>
2017-04-02 01:27:18 +0300

    http try get data from remote server

--- HolidayAdvisor/HolidayAdvisor/HolidayAdvisor/AppDelegate.swift
+++ HolidayAdvisor/HolidayAdvisor/HolidayAdvisor/AppDelegate.swift
@@ -12,7 +12,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    var baseURL: String = "https://holiday-advisor.herokuapp.com/api"
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
--- HolidayAdvisor/HolidayAdvisor/HolidayAdvisor/HomeViewController.swift
+++ HolidayAdvisor/HolidayAdvisor/HolidayAdvisor/HomeViewController.swift
@@ -10,9 +10,30 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var placesLabel: UILabel!
+    var url : URL{
+        get{
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            return URL(fileURLWithPath: appDelegate.baseURL + "/places")
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let request = NSMutableURLRequest(url: self.url)
+        request.httpMethod = "GET"
+        let session = URLSession.shared.dataTask(with: request as URLRequest){(data, response, error) in
+            do{
+                let jsonData =  try JSONSerialization.jsonObject(with: data!, options: .allowFragments)
+//                self.placesLabel.text = String(describing: jsonData)
+                print(jsonData)
+                
+            }catch{
+                
+            }
+        }
+        
+        session.resume()
     }
 
     override func didReceiveMemoryWarning() {
