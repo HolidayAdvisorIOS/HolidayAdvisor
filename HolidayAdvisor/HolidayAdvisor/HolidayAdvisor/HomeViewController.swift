8c39286751d4c43b323897cc4c2a0deb44b30988
Iliyan Gogov <iliangogov@gmail.com>
2017-04-01 18:03:26 +0300

    Initial commit

---
+++ HolidayAdvisor/HolidayAdvisor/HolidayAdvisor/HomeViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HolidayAdvisor
+//
+//  Created by Iliyan Gogov on 4/1/17.
+//  Copyright © 2017 Iliyan Gogov. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ HolidayAdvisor/HolidayAdvisor/Pods/Target Support Files/Pods-HolidayAdvisor/Pods-HolidayAdvisor-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HolidayAdvisor : NSObject
+@end
+@implementation PodsDummy_Pods_HolidayAdvisor
+@end
