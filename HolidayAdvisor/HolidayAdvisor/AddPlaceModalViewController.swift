bf18153ad915b63b1d78fe775baf4414316f1a02
Iliyan Gogov <iliangogov@gmail.com>
2017-04-02 11:56:43 +0300

    Sample data received from remote server

---
+++ HolidayAdvisor/HolidayAdvisor/AddPlaceModalViewController.swift
@@ -0,0 +1,9 @@
+//
+//  AddPlaceModalViewController.swift
+//  HolidayAdvisor
+//
+//  Created by Iliyan Gogov on 4/2/17.
+//  Copyright © 2017 Iliyan Gogov. All rights reserved.
+//
+
+import Foundation
